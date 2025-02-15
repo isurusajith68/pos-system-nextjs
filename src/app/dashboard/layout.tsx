541f14a153804adf781043be72765461e4fb655a
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2025-02-15 23:31:53 +0530

    fix: update layout component for improved height calculation and responsiveness

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,13 +11,13 @@ interface LayoutProps {
 
 const layout = async ({ children }: LayoutProps) => {
   return (
-    <div className="flex sm::h-screen flex-col h-svh">
-      <NavBar  />
+    <div className="flex h-screen flex-col">
+      <NavBar />
       <div className="flex flex-1">
         <Sidebar />
         <ScrollArea
           className="flex-1 p-4 overflow-y-auto rounded-tl-2xl bg-secondary"
-          style={{ height: "calc(100vh - 60px)" }}
+          style={{ height: "calc(100dvh - 60px)" }}
         >
           {children}
         </ScrollArea>
