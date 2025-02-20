c73c5b400c0ac6d32d56d08cb08e54200940ead2
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2025-02-20 21:57:23 +0530

    feat: update Navbar to display printer status with icons and change logo image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,6 +7,8 @@ import { getUserFromCookie } from "@/services/auth";
 import { redirect } from "next/navigation";
 import { IoReload, IoReloadCircle } from "react-icons/io5";
 import { set } from "date-fns";
+import { Printer } from "lucide-react";
+import Image from "next/image";
 
 export default function Navbar() {
   const [darkMode, setDarkMode] = useState(false);
@@ -61,6 +63,8 @@ export default function Navbar() {
           setTimeout(() => {
             setSpin(false);
           }, 1000);
+
+          setPrinterOnline(false);
         }
       } catch (error) {
         console.error("Error fetching printer status:", error);
@@ -79,30 +83,34 @@ export default function Navbar() {
     <header className="sm:h-16 h-14 flex justify-between items-center px-6 bg-background pt-2 pl-5 p-1">
       <div className="flex items-center gap-5">
         <div className="flex items-center">
-          {/* <Image
-            src="/loho.png"
+          <Image
+            src="/favicon.png"
             width={35}
             height={35}
             alt="logo"
             className="mr-2 rounded-full"
-          /> */}
-          <span className="sm:text-lg  font-extrabold  font-[family-name:var(--font-geist-sans)] text-primary">
-            POS SYSTEM
+          />
+          <span className="sm:text-lg uppercase font-extrabold  font-[family-name:var(--font-geist-sans)] text-primary">
+            The Cadbury
           </span>
         </div>
       </div>
 
       <div className="flex items-center gap-4">
-        <div className="p-2 rounded-full bg-white dark:bg-muted text-muted-foreground hover:bg-accent hover:text-accent-foreground transition duration-200 ease-in-out flex gap-2">
+        <div
+          onClick={() => setCount(count + 1)}
+          className="p-2 rounded-full hidden sm:flex bg-white dark:bg-muted text-muted-foreground hover:bg-accent hover:text-accent-foreground transition duration-200 ease-in-out  gap-2"
+        >
           {printerOnline ? (
-            <span className="text-green-500 text-sm">Printer Online</span>
+            <span className="text-green-500 text-xs flex items-center gap-2">
+              <Printer size={16} /> Online
+            </span>
           ) : (
-            <span className="text-red-500  text-sm">Printer Offline</span>
+            <span className="text-red-500 text-xs flex items-center gap-2">
+              <Printer size={16} /> Offline
+            </span>
           )}
-          <IoReloadCircle
-            onClick={() => setCount(count + 1)}
-            className={spin ? "animate-spin" : ""}
-          />
+          <IoReloadCircle className={spin ? "animate-spin" : ""} size={20} />
         </div>
         <button
           onClick={toggleDarkMode}
@@ -116,9 +124,9 @@ export default function Navbar() {
           )}
         </button>
 
-        <div className="flex items-center cursor-pointer border-l border-border pl-4 gap-4">
+        <div className="flex items-center cursor-pointer border-l border-border sm:pl-4 gap-4">
           <div className="flex flex-col items-end  ml-2 ">
-            <span className="ml-2  font-semibold text-foreground text-sm sm:text-base">
+            <span className="ml-2  font-semibold text-foreground text-xs sm:text-base">
               {user?.name}
             </span>
             <span className="ml-2 font-thin sm:text-xs text-[0.6rem]">
